ff6d5529893036e87d53cc6abed6b1cc14ad5fd8
= <=>
2022-02-08 23:11:17 -0500

    terminacion basica del proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import FormularioColores from './components/FormularioColores.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='contenedorPrincipal'>
+      <div className="contenedorDeElementos">
+        <h1 className='tituloPrincipal'>Visualizador de Colores</h1>
+        <FormularioColores></FormularioColores>
+      </div>
     </div>
   );
 }
---
+++ src/components/FormularioColores.js
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+
+export default function FormularioColores() {
+    const [color,setColor] = useState('0')
+
+    function cambioDeColor(e) {
+        const input = document.querySelector('.contenedorPrincipal')
+        setColor(e.target.value)
+        input.style.backgroundColor = color
+    }
+
+    return (
+        <div className="contenedorInput">
+            <label htmlFor='inputColor' id='labelInput'>Seleccione el color de su gusto: </label>
+            <input type={"color"} id="inputColor" onChange={cambioDeColor} ></input>
+        </div>
+    )
+}
